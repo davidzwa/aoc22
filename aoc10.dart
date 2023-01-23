fb65930361efc4eb1330b59597d068fd31256b96
David Zwart <davidzwa@gmail.com>
2023-01-23 23:04:49 +0100

    feat: implement part 2 of aoc10

--- aoc10.dart
+++ aoc10.dart
@@ -1,37 +1,85 @@
 import 'dart:io';
 
+bool decide(int reg, int index) {
+  int col = index + 1;
+  return index == reg || index == reg + 1 || index == reg - 1;
+}
+
+void printScreen(List<List<String>> screen) {
+  for (var i in screen) {
+    print(i.join(""));
+  }
+}
+
+class Point {
+  var register = 1;
+  var col = 0;
+  var row = 0;
+  int width;
+  Point(this.width);
+  void moveNext() {
+    if ((col + 1) % width == 0) {
+      row++;
+      col = 0;
+      print("Row change ${col}");
+    } else {
+      col++;
+    }
+  }
+}
+
 void main() {
   String path = 'questions/aoc10_input';
   File file = File(path);
   List<String> lines = file.readAsLinesSync();
-
-  // 20, 60, 100, 140, 180, 220
   final arr = <int>[20, 60, 100, 140, 180, 220];
-  var values = [1];
-  // print('Hello, World! ${arr}');
+
+  int w = 40;
+  int h = 6;
+  var screen = List.generate(
+      h, (i) => List.generate(w, (i) => '.', growable: false),
+      growable: false);
+
+  var p = Point(w);
+  var values = [p.register];
   var index = 0;
-  print('[${index}] Last ${values.last}');
   for (var line in lines) {
-    if (line == 'noop') {
+    print('[${p.row},${p.col}] EXEC ${line} REG ${p.register}');
+    if (line.contains('addx')) {
+      // Step 1
+      screen[p.row][p.col] = decide(p.register, p.col) ? '#' : '.';
+
+      // Step 2
       values.add(values.last);
       index += 1;
-    } else if (line.contains('addx')) {
-      values.add(values.last);
+      p.moveNext();
+      screen[p.row][p.col] = decide(p.register, p.col) ? '#' : '.';
+
+      // Instruction execution complete
       var count = int.parse(line.replaceFirst('addx ', ''));
-      values.last += count;
+      var newVal = values.last + count;
+      values.add(newVal);
+      p.register = newVal;
+      p.moveNext();
+      index += 1;
+    } else if (line == 'noop') {
       values.add(values.last);
-      index += 2;
+      index += 1;
+      screen[p.row][p.col] = decide(p.register, p.col) ? '#' : '.';
+      p.moveNext();
     }
 
-    print('[${index}] Last ${values.last}');
+    print('[${p.row},${p.col}] DONE ${line} REG ${p.register} I ${index}');
   }
 
+  // Part 1 of AOC10
   var sum = 0;
   for (var i in arr) {
-    var val = values[i - 2] * i;
+    var val = values[i - 1] * i;
     print('${values[i - 1]} * ${i} = ${val}');
-
     sum += val;
   }
   print('${values.length} ${values.last} sum: ${sum}');
+
+  printScreen(screen);
 }
