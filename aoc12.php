bed12e3e69d72f81f779cb53604141fe66152239
David Zwart <davidzwa@gmail.com>
2023-03-04 15:25:47 +0100

    feat: rewrite minheap

--- aoc12.php
+++ aoc12.php
@@ -1,5 +1,7 @@
 <?php
 
+global $BR;
+$BR = "<br/>";
 class Coord
 {
   public int $x;
@@ -26,6 +28,11 @@ class Coord
     $this->parent = $parent;
   }
 
+  public function __compareTo($other): int
+  {
+    return $this->f - $other->f;
+  }
+
   // https://wiki.php.net/rfc/object-comparison
   public function __equals($other): bool
   {
@@ -35,7 +42,24 @@ class Coord
   public function __toString(
   )
   {
-    return "[{$this->x},{$this->y}] F:{$this->f} G:{$this->g} H:{$this->h} <br/>";
+    return "({$this->x}, {$this->y}) <br/>&nbsp;f{$this->f} g{$this->g} h{$this->h} <br/>";
+  }
+
+  public function chr()
+  {
+    if ($this->val == 0) {
+      return 'S';
+    } else if ($this->val == 97) {
+      return 'E';
+    }
+
+    return chr($this->val + 96);
+  }
+
+  public function debug(
+  )
+  {
+    return "({$this->x}, {$this->y})";
   }
 }
 
@@ -52,7 +76,35 @@ function printMatrix($matrix)
   }
 }
 
-$lines = file('./questions/aoc12_input');
+function printHeap(\Ds\Deque $heap)
+{
+  global $BR;
+
+  echo "HeapCnt " . $heap->count() . $BR;
+  $index = 0;
+  foreach ($heap as $el) {
+    global $BR;
+    echo "{$index}: " . $el->debug() . " {$el->f}" . $BR;
+
+    $index++;
+  }
+}
+function insertHeap(\Ds\Deque $heap, Coord $coord)
+{
+  $index = 0;
+  while ($index < $heap->count()) {
+    $not_found = $heap->get($index)->f < $coord->f;
+    if (!$not_found)
+      break;
+
+    $index++;
+  }
+
+  $heap->insert($index, $coord);
+}
+
+
+$lines = file('./questions/aoc12_test_input');
 // Char values offset by -97 (to make them zero based)
 $m = array();
 $start = null;
@@ -96,32 +148,34 @@ foreach ($lines as $line) {
 // https://en.wikipedia.org/wiki/A*_search_algorithm
 // https://www.geeksforgeeks.org/a-search-algorithm/
 // Prepare the A-star algorithm
-$openSet = new SplMinHeap();
+$openSet = new \Ds\Deque();
 $cameFrom = [];
 $rows = count($m);
 $cols = count($m[0]);
-echo "S:{$start}";
-echo "E:{$end}";
+echo "Start{$start}";
+echo "End{$end}";
 // Add the start node with its priority f (=0)
-$openSet->insert([$start->f, $start]);
+insertHeap($openSet, $start);
+printHeap($openSet);
 
 $iter = 0;
-while ($openSet->count() != 0 && $iter < 10) {
-  echo "Iter {$iter}<br/><br/>";
+while ($openSet->count() != 0 && $iter < 3) {
+  echo "<br/>---- Iteration {$iter} (openSet {$openSet->count()}) ----<br/>";
 
   // Pop node with least f from the openSet list, call it bestNode, 
-  $bestNode = $openSet->extract()[1]; // Drop the prio value, keep the elem (with f attrib.)
+  $bestNode = $openSet->remove(0); // Drop the prio value, keep the elem (with f attrib.)
   // Push to closed set
   array_push($cameFrom, $bestNode);
 
   // Check end condition
   if ($bestNode == $end) {
-    echo "End found:{$end}<br/><br/>";
+    echo "End found:{$end}<br/>";
     break;
   }
   $currVal = $bestNode->val;
 
   // generate bestNode's top/left/right/bottom successors and set their parents to bestNode
+  echo "Generating children<br/>";
   $row = $bestNode->x;
   $col = $bestNode->y;
   $children = array();
@@ -147,59 +201,54 @@ while ($openSet->count() != 0 && $iter < 10) {
     array_push($children, [$x, $y]);
   }
 
-  $valchr = chr($currVal + 96);
+  $bestf = $bestNode->f;
   $children_count = count($children);
-  echo "Looping children, curr val: {$valchr} count ${children_count}<br/>";
+  echo "> Looping ${children_count} children, current char: {$bestNode->chr()} (pos{$bestNode->debug()}, f={$bestf})<br/>";
 
   foreach ($children as $coords) {
     $x = $coords[0];
     $y = $coords[1];
     $val = $m[$x][$y];
     $child = new Coord($x, $y, $val);
+    // echo "- Next child {$child->debug()} <br/>";
 
     // End condition (probably duplicate)
     if ($child->isGoal()) {
-      echo "Goal found X:{$child->x},Y:{$child->y}";
+      echo "- Goal found {$child->debug()}";
       return;
     }
 
     // If node is equal to one from closed, skip
     if (in_array($child, $cameFrom)) {
-      echo "Skipping closed child";
+      echo "- Skipping closed child {$child->debug()}";
       continue;
     }
 
     // (Re-)calculate node potential/heuristics
     $child->g = $bestNode->g + 1;
-    echo "best g {$bestNode->g}<br/>";
-    echo "new g {$child->g}<br/>";
-    // Manhattan heuristics
-    $child->h = abs($child->x - $end->x) + abs($child->y - $end->y);
+    // echo "Parent {$bestNode->debug()} g={$bestNode->g}<br/>";
+    $xdiff = $child->x - $end->x;
+    $ydiff = $child->y - $end->y;
+    $child->h = abs($xdiff) + abs($ydiff);
     $child->f = $child->g + $child->h;
 
-    echo "child computed <br/>";
-    echo $child;
+    echo "- Child {$child->debug()} (xd:{$xdiff} yd:{$ydiff}) computed f{$child->f} = g{$child->g} + h{$child->h}<br/>";
+    // echo "OpenSet count {$openSet->count()} <br/>";
 
     // if child is in openList only add it when its better
     foreach ($openSet as $elem) {
       if ($child == $elem && $child->g > $elem->g) {
-        echo "Skipping child <br/>";
+        echo "XX Skipping child <br/>";
         continue 2;
       }
     }
 
-    $openSet->insert([$child->f, $child]);
+    echo "++ Adding child to openSet (f{$child->f}) <br/>";
+
+    insertHeap($openSet, $child);
+    printHeap($openSet);
   }
 
   $iter += 1;
 }
-
-// printMatrix($m);
-// $bestNode = new SplQueue();
-// $bestNode[] = 1;
-// $bestNode[] = 2;
-// $bestNode[] = 3;
-// foreach ($bestNode as $elem) {
-//     echo $elem . "\n";
-// }
 ?>
